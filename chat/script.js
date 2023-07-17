7c589c0f4ec34d20cbfd30836af12144a941deee
fudge21 <113610178+fudge21@users.noreply.github.com>
2023-07-16 20:40:00 -0400

    test

--- chat/script.js
+++ chat/script.js
@@ -2,6 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+  import { GoogleAuthProvider, getAuth, signInWithPopup  } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +21,34 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  const auth = getAuth();
+
+  const provider = new GoogleAuthProvider();
+
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  auth.useDeviceLanguage();
+
+  provider.setCustomParameters({
+    'login_hint': 'user@example.com'
+  });
+
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user)
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
